618ff51dbee11b48739968cfd0f88ac08a0216a2
hviwen <Hankins.Home@outlook.com>
2025-09-11 10:14:23 +0800

    feat: sort byhand 0911

--- stack/sorting/byHand.js
+++ stack/sorting/byHand.js
@@ -3,8 +3,25 @@
  * @param {*[]} arr
  * @returns {[]|*}
  */
-function quickSort(arr) {
+function quickSort(arr = []) {
+    if (arr.length <= 1) return arr
 
+    let piovt = arr[Math.floor(arr.length / 2)]
+    const left = []
+    const middle = []
+    const right = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === piovt) {
+            middle.push(arr[i])
+        } else if (arr[i] < print) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+
+    return [...quickSort(left), ...middle, ...quickSort(right)]
 }
 
 /**
@@ -13,7 +30,19 @@ function quickSort(arr) {
  * @returns {[]|*}
  */
 function bubbleSort(arr) {
+    const n = arr.length
+    for (let i = 0; i < n; i++) {
+        let flag = false
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+                flag = true
+            }
+        }
+        if (!flag) break
+    }
 
+    return arr
 }
 
 /**
@@ -22,7 +51,20 @@ function bubbleSort(arr) {
  * @returns {[]|*}
  */
 function selectSort(arr) {
-  
+    const n = arr.length
+    for (let i = 0; i < n - 1; i++) {
+        let miniIndex = i
+
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[miniIndex]) {
+                miniIndex = j
+            }
+        }
+        if (arr[i] !== arr[miniIndex]) {
+            [arr[i], arr[miniIndex]] = [arr[miniIndex], arr[i]]
+        }
+    }
+    return arr
 }
 
 /**
@@ -31,7 +73,19 @@ function selectSort(arr) {
  * @returns {[]|*}
  */
 function insertSort(arr) {
-  
+    for (let i = 1; i < arr.length; i++) {
+        const current = arr[i]
+        let j = i - 1
+
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j]
+            j--
+        }
+
+        arr[j + 1] = current
+    }
+
+    return arr
 }
 
 /**
@@ -40,7 +94,28 @@ function insertSort(arr) {
  * @returns {[]|*}
  */
 function mergeSort(arr) {
-  
+    if (arr.length <= 1) return arr
+    const middle = Math.floor(arr.length / 2)
+    const left = mergeSort(arr.slice(0, middle))
+    const right = mergeSort(arr.slice(middle))
+
+    return mergify(left, right)
+
+    function mergify(left, right) {
+        const result = []
+        let leftIndex = 0
+        let rightIndex = 0
+
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] <= right[rightIndex]) {
+                result.push(left[leftIndex++])
+            } else {
+                result.push(right[rightIndex++])
+            }
+        }
+
+        return result.concat(left.slice(leftIndex), right.slice(rightIndex))
+    }
 }
 
 /**
@@ -49,7 +124,37 @@ function mergeSort(arr) {
  * @returns {[]|*}
  */
 function heapSort(arr) {
-  
+    const n = arr.length
+
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+        heapify(arr, n, i)
+    }
+
+    for (let i = n - 1; i > 0; i--) {
+        [arr[i], arr[0]] = [arr[0], arr[i]]
+        heapify(arr, i, 0)
+    }
+
+    return arr
+
+    function heapify(arr, heapSize, rootIndex) {
+        let largest = heapSize
+        let left = rootIndex * 2 + 1
+        let right = rootIndex * 2 + 2
+
+        if (left < heapSize && arr[left] > arr[largest]) {
+            largest = left
+        }
+
+        if (right < heapSize && arr[right] > arr[largest]) {
+            largest = right
+        }
+
+        if (largest !== rootIndex) {
+            [arr[largest], arr[rootIndex]] = [arr[rootIndex], arr[largest]]
+            heapify(arr, heapSize, largest)
+        }
+    }
 }
 
 /**
@@ -58,5 +163,25 @@ function heapSort(arr) {
  * @returns {[]}
  */
 function countSort(arr) {
-  
+    const n = arr.length
+    const max = Math.max(...arr)
+    const min = Math.min(...arr)
+    const range = max - min - 1
+
+    const counts = Array(range).fill(0)
+
+    for (let i = 0; i < arr.length; i++) {
+        counts[arr[i] - min]++
+    }
+
+    const result = []
+    for (let i = 0; i < counts.length; i++) {
+        while (counts[i] > 0) {
+            result.push(i + min)
+            counts[i]--
+        }
+    }
+
+    return counts
+
 }
